e6d80b18a1c58d8ec9d8e1b7a863fc4d2c384726
Gabriel Kigundu <dadidiesel@gmail.com>
2018-01-18 02:47:24 -0500

    Update and rename tree_name_switch.py to name_switcher.py

---
+++ name_switcher.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+#name_switcher.py
+#By: Gabriel Kigundu
+#gabrielkigundu@gmail.com
+#Kennesaw State University
+#September 27, 2016
+
+docstring= """
+#name_switch.py
+#By: Gabriel Kigundu
+#gabrielKigundu@gmail.com
+#Kennesaw State University
+#September 27, 2016
+DESCRIPTION
+switch accession with provided accession or given provided a tab delimited file with accession, name as first 2 columns
+"""
+"""
+USAGE:
+python name_switch.py -i <fasta-file> -a <accession-file> -o <outfile>
+output is optional
+input required as file or stdin
+"""
+
+import sys
+import argparse
+from argparse import RawDescriptionHelpFormatter
+import os
+
+inFile=''
+writeFile = False
+switched=[]
+
+parser = argparse.ArgumentParser(description=docstring, formatter_class=RawDescriptionHelpFormatter)
+parser.add_argument('-o', '-out', '--output', help='Output file name', required=False)
+parser.add_argument('-a', '-acc', '--accession', help='Accession file name', required=True)
+#set input file to stdin else get input file name
+if not sys.stdin.isatty():
+    inFile = sys.stdin
+else:
+    parser.add_argument('-i','-in','--input', help='Input file name',required=True)
+    inFile = parser.parse_args().input
+	if not os.path.exists(inFile):
+		sys.exit("Input file does not exist!!")
+    
+args = parser.parse_args()
+
+if args.output is not None:
+    writeFile = True
+	if not os.path.exists(args.accession):
+		sys.exit("Output file does not exist")
+   
+#add accessions to dictionary
+if not os.path.exists(args.accession):
+		sys.exit("Input file does not exist!!")
+nameSet ={}
+with open(args.accession, 'r') as nameFile:
+	for line in nameFile:
+		line=line.strip().split("\t")
+		if len(line) > 1:
+			if len(line[0]) > 0:
+				if line[1] in nameSet:
+					sys.exit("Accessions listed multiple times in accession file")
+				else:
+					nameSet[line[0]] = line[1]
+
+#read input file line by line and replace all occurrences of accessions in file
+with open(inFile, 'r') as infile:
+	for line in infile:
+		if len(line)>0:
+			for accession in nameSet:
+				index = line.find(accession)
+				if index !=-1:
+					line = line[:index] + nameSet[accession] + line[index+len(accession):]
+			switched.append(line)
+#write to file or print to screen
+if writeFile:
+	with open(args.output, 'w') as outFile:
+		for line in switched:
+			outFile.write(line)
+	sys.exit("name_switch.py complete" )
+else:
+    for line in switched:
+        print line.strip()
