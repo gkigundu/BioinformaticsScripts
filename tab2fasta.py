280c0701d9a51c492b5440e0ec8ff3ed2461b11b
Gabriel Kigundu <dadidiesel@gmail.com>
2018-01-18 10:44:39 -0500

    Update tab2fasta.py

--- tab2fasta.py
+++ tab2fasta.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python
+#! python3
+#!/usr/bin/env python3
 #tab2fasta.py
 #By: Gabriel Kigundu
 #gabrielKigundu@gmail.com
@@ -11,20 +12,14 @@ docstring= """
 #gabrielKigundu@gmail.com
 #Kennesaw State University
 #September 27, 2016
-
 DESCRIPTION
-
 Converts tab delimited to FASTA
-
 This script converts a 2 column (text based) tab delimited file to a fasta file.
 The first column is assumed to be the header and second column is the sequence.
 Max fasta sequence length will be 120 characters per line.
 
-"""
-"""
 USAGE:
-python tab2fasta.py -i <tab-file> -o <outfile>
-
+python tab2fasta.py -i <tab-file> -o <fasta-file>
 output is optional
 input required as file or stdin
 """
@@ -32,67 +27,50 @@ input required as file or stdin
 import sys
 import argparse
 from argparse import RawDescriptionHelpFormatter
+import os
 
-infile=''
-outfile=''
-readfile=False
-writefile = False
+inFile=''
 
 parser = argparse.ArgumentParser(description=docstring, formatter_class=RawDescriptionHelpFormatter)
 parser.add_argument('-o', '-out', '--output', help='Output file name', required=False)
 
 ## show values ##
 if not sys.stdin.isatty():
-    infile = sys.stdin
+    inFile = sys.stdin
     parser.add_argument('-i','-in','--input', help='Input file name',required=False)
 else:
     parser.add_argument('-i','-in','--input', help='Input file name',required=True)
-    readfile = True
-    infile = open(parser.parse_args().input, 'r')
+    inFile = parser.parse_args().input
+    if not os.path.exists(inFile):
+        sys.exit('Input file not found')
     
 args = parser.parse_args()
 
+#read file, check for 2 columns, append data to list
+output=[]
+with open(inFile, 'r') as infile:
+    for line in infile:
+        line= line.strip().split('\t')
+        if len(line) == 1:
+            print (line)
+            sys.exit("input file has less than 2 columns")
+        if len(line)>1:
+            line[0]= '>' + line[0]
+            output.append(line[0])
+            seq = ''
+            count = 100
+            while len(line[1]) > count:
+                output.append(line[1][count-100:count])
+                count += 100
+            count-=100
+            if len(line[1]) > count:
+                output.append(line[1][count:])
+#print to screen or write to file
 if args.output is not None:
-    writefile = True
-    outfile = open(args.output, 'w')
-
-
-for line in infile:
-    line= line.strip().split('\t')
-    if len(line) < 2:
-        print line
-        #sys.exit("input has less than 2 columns")
-    print line
-    if len(line)>1:
-        count = 1
-        header= '>' + line[0]
-        while count < len(line)-1:
-            header += ' ' + line[count]
-            count +=1
-        print header
-        if writefile:
-            outfile.write(header+'\n')
-        else:
-            print(header)
-        seq = len(line)-1
-        while len(line[seq]) > 120:
-            if writefile:
-                outfile.write(line[seq][:119]+'\n')
-                line[seq]= line[seq][120:]
-            else:
-                print(line[seq][:119])
-                line[seq]= line[seq][120:]
-        if len(line[seq]) != 0:
-            if writefile:
-                outfile.write(line[seq]+'\n')
-            else:
-                print line[seq]
-            line[seq] = ''
-
-if readfile:
-    print ("Input file: %s" % args.input )
-    infile.close()
-if writefile:
-    print ("Output file: %s" % args.output )
-    outfile.close()
-sys.exit("tab2fasta.py complete" )
+    with open(args.output, 'w') as outFile:
+        for line in output:
+            outFile.write(line + '\n')  
+    sys.exit("tab2fasta.py complete" )
+else:
+    for line in output:
+        print(line)
